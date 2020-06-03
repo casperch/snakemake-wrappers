cbaeca09c84e3aebb9a065d675f57b6a8cbad511
casperch <casperch@gmail.com>
2020-06-03 15:46:42 +0900

    vcftools plink

---
+++ bio/vcftools/plink/wrapper.py
@@ -0,0 +1,28 @@
+__author__ = "Jae-Pil Choi"
+__copyright__ = "Copyright 2020, Jae-Pil Choi"
+__email__ = "casperch@gmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+import os
+
+input_flag = "--vcf"
+if snakemake.input[0].endswith(".gz"):
+    input_flag = "--gzvcf"
+
+output = os.path.splitext(snakemake.output[0])[0]
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+extra = snakemake.params.get("extra", "")
+
+shell(
+    "vcftools "
+    "{input_flag} "
+    "{snakemake.input} "
+    "{extra} " 
+    "--plink "    
+    "--out {output} "
+    "{log}"
+)
