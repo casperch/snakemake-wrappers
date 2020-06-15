746c7039fc29e87dc724c0b297fca11b67d909ee
casperch <casperch@gmail.com>
2020-06-15 10:34:19 +0900

    racon

---
+++ bio/racon/wrapper.py
@@ -0,0 +1,26 @@
+"""Snakemake wrapper for Racon Consensus module for raw de novo DNA assembly of long uncorrected reads."""
+
+__author__ = "Jae-Pil Choi"
+__copyright__ = "Copyright 2020, Jae-Pil choi"
+__email__ = "casperch@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+from os import path
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+extra = snakemake.params.get("extra", "")
+match = snakemake.params.get("match", 8)
+mismatch = snakemake.params.get("mismatch", -6)
+gap = snakemake.params.get("gap", -8)
+window_length = snakemake.params.get("window_length", -8)
+
+shell(
+    "racon -t {snakemake.threads} "
+    " -m {match} "
+    " -x {missmatch} "
+    " -g {gap} "
+    " -w {window_length} "
+    " {extra} "
+    "{snakemake.input.sequences} {snakemake.input.overlaps} {snamemake.input.target_sequences} > {snakemake.output} {log}"
+)
